b3b8912a9500abbfa24d1f4914be159623d2374c
Onur Çankaya <onur.eren.cankaya@gmail.com>
2017-02-14 08:43:17 +0100

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+var gulp = require('gulp');
+
+// styles
+gulp.task('styles', function() {
+  console.log('starting styles task');
+});
+
+// scripts
+gulp.task('scripts', function() {
+  console.log('starting scripts task');
+});
+
+// images
+gulp.task('images', function() {
+  console.log('starting images task');
+});
+
+gulp.task('default', function() {
+  console.log('starting default task');
+});
