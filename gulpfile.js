6ab8707bf4a0bddab1ac57d900f66566a0201ea7
Onur Çankaya <onur.eren.cankaya@gmail.com>
2017-02-17 10:16:45 +0100

    Switch css to scss

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,40 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var livereload = require('gulp-livereload');
-var concat = require('gulp-concat');
-var minify = require('gulp-clean-css');
+// var concat = require('gulp-concat');
+// var minify = require('gulp-clean-css');
 var autoprefixer = require('gulp-autoprefixer');
 var plumber = require('gulp-plumber');
 var sourcemaps = require('gulp-sourcemaps');
+var sass = require('gulp-sass');
 
 // file paths
 var DIST_PATH = 'public/dist';
 var SCRIPTS_PATH = 'public/scripts/**/*.js';
-var STYLES_PATH = 'public/styles/**/*.css';
+// var STYLES_PATH = 'public/styles/**/*.css';
 
 // styles
+// gulp.task('styles', function() {
+//   console.log('starting styles task');
+//   return gulp.src(['public/styles/reset.css', STYLES_PATH])
+//     .pipe(plumber(function(err) {
+//       console.log('styles task error');
+//       console.log(err);
+//       this.emit('end'); // internal gulp method that tells gulp to stop running the rest of the process but still keeps the gulp up
+//     }))
+//     .pipe(sourcemaps.init())
+//     .pipe(autoprefixer())
+//     .pipe(concat('styles.css'))
+//     .pipe(minify())
+//     .pipe(sourcemaps.write())
+//     .pipe(gulp.dest(DIST_PATH))
+//     .pipe(livereload());
+// });
+
+// styles for scss
 gulp.task('styles', function() {
   console.log('starting styles task');
-  return gulp.src(['public/styles/reset.css', STYLES_PATH])
+  return gulp.src('public/scss/styles.scss')
     .pipe(plumber(function(err) {
       console.log('styles task error');
       console.log(err);
@@ -23,8 +42,9 @@ gulp.task('styles', function() {
     }))
     .pipe(sourcemaps.init())
     .pipe(autoprefixer())
-    .pipe(concat('styles.css'))
-    .pipe(minify())
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(DIST_PATH))
     .pipe(livereload());
@@ -54,5 +74,6 @@ gulp.task('watch', function() {
   require('./server.js');
   livereload.listen();
   gulp.watch(SCRIPTS_PATH, ['scripts']);
-  gulp.watch(STYLES_PATH, ['styles']);
+  // gulp.watch(STYLES_PATH, ['styles']);
+  gulp.watch('public/scss/**/*.scss', ['styles']);
 });
